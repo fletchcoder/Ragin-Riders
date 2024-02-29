8ffdd635fcc64dbf9cee44563c1a4f6cec52a6d4
fletchcoder <131314453+fletchcoder@users.noreply.github.com>
2024-02-28 16:58:58 -0800

    Updated header, footer, home page
    Added sign in and cart to right side of header
    Changed CSS of footer and home page to be responsive on tablet size resolutions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,33 +6,43 @@ export default function Header() {
 	return (
 		<>
 			<div className={styles.header}>
-				<Link prefetch={false} href={"/"}>
-					<Image
-						src={"/icon.ico"}
-						height={50}
-						width={50}
-						alt={`Header PNG Image for Ragin' Riders`}></Image>
-				</Link>
-				<nav>
-					<ul>
-						<li>
-							<Link prefetch={false} href={"/shop"}>
-								Shop
-							</Link>
-						</li>
-						<li>
-							<Link prefetch={false} href={"/contact"}>
-								Contact
-							</Link>
-						</li>
-						<li>
-							<Link prefetch={false} href={"/join"}>
-								Join Us
-							</Link>
-						</li>
-					</ul>
-				</nav>
+				<div className={styles.left}>
+					<Link prefetch={false} href={"/"}>
+						<Image
+							src={"/icon.ico"}
+							height={50}
+							width={50}
+							alt={`Header PNG Image for Ragin' Riders`}></Image>
+					</Link>
+					<nav>
+						<ul>
+							<li>
+								<Link prefetch={false} href={"/shop"}>
+									Shop
+								</Link>
+							</li>
+							<li>
+								<Link prefetch={false} href={"/contact"}>
+									Contact
+								</Link>
+							</li>
+							<li>
+								<Link prefetch={false} href={"/join"}>
+									Join Us
+								</Link>
+							</li>
+						</ul>
+					</nav>
+				</div>
+
 				<div className={styles.right}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24">
+						<path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z" />
+					</svg>
 					<p>Sign In</p>
 				</div>
 			</div>
